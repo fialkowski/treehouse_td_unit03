453c88a90f5934eecd3f67190b11308b2f8d1a2a
Nick Fialkowski <nickolai.fialkowski@gmail.com>
2019-02-14 22:46:38 -0500

    Slightly developed the app structure
    added the QuizGame file and GameViewController class

---
+++ BoutTime/GameViewController.swift
@@ -0,0 +1,36 @@
+//
+//  GameViewController.swift
+//  BoutTime
+//
+//  Created by nikko444 on 2019-02-14.
+//  Copyright © 2019 nikko444. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    @IBOutlet weak var firstRowLabel: UILabel!
+    @IBOutlet weak var secondRowLabel: UILabel!
+    @IBOutlet weak var thirdRowLabel: UILabel!
+    @IBOutlet weak var fourthRowLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BoutTime/QuizGame.swift
@@ -0,0 +1,13 @@
+//
+//  QuizGame.swift
+//  BoutTime
+//
+//  Created by nikko444 on 2019-02-14.
+//  Copyright © 2019 nikko444. All rights reserved.
+//
+
+import Foundation
+
+protocol QuizGame {
+    
+}
